626932bccbb5d8cce5e0550788dd40768c4c6518
UTSUBO Katsuhiko <122595462+katutsubo@users.noreply.github.com>
2025-01-29 16:02:28 +0900

    modify source code comments

--- samples/vue3/src/components/KanjiKana.vue
+++ samples/vue3/src/components/KanjiKana.vue
@@ -1,5 +1,5 @@
 <!--
- Copyright (c) 2024 デジタル庁
+ Copyright (c) 2025 デジタル庁
  
  This software is released under the MIT License.
  https://opensource.org/licenses/MIT
@@ -48,13 +48,13 @@
             <input type="checkbox" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="detail"  />
 
         </div>
-        <div>
+        <div  class="grid gap-6 mb-6 md:grid-cols-2">
             <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" @click="calc()">突合！</button>
         </div>
 
         <div class="grid gap-6 mb-6 md:grid-cols-2">
-            <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">結果</label>
-            <div class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" >{{  result  }}</div>
+            <label  class="block mb-2 text-sm font-medium text-red-900 dark:text-white">結果</label>
+            <div class="block mb-2 text-sm font-medium text-red-900 dark:text-white" >{{  result  }}</div>
         </div>
         <div class="grid gap-6 mb-6 md:grid-cols-2">
             <label  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">レスポンス</label>
